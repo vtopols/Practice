10900497946d6cd9a7206660a2ddb694185ea9be
vtopols <101529916+vtopols@users.noreply.github.com>
2026-05-18 13:36:26 +0300

    Delete TaskManager directory

--- TaskManager/src/main/java/org/example/taskmanager/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.taskmanager.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- TaskManager/src/main/java/org/example/taskmanager/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.taskmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByAccountId(Long accountId);
-}
