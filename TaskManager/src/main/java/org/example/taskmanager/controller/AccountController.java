6a74969c091e72670c42dc5078b9449f50daad79
vtopols <101529916+vtopols@users.noreply.github.com>
2026-05-18 12:55:25 +0300

    Update AccountController.java

--- TaskManager/src/main/java/org/example/taskmanager/controller/AccountController.java
+++ TaskManager/src/main/java/org/example/taskmanager/controller/AccountController.java
@@ -1,4 +1,4 @@
-package org.example.taskmanager;
+package org.example.taskmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
